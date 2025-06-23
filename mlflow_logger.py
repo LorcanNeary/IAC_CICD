176da072240e43e89728076f0c1acfd280a8d9e5
Lorcan Neary <lneary@bcis.co.uk>
2025-06-23 20:29:10 +0100

    added in a parser to pick up the password parameter

--- mlflow_logger.py
+++ mlflow_logger.py
@@ -1,16 +1,27 @@
 import mlflow
 import os
+import argparse
 
 server = "az-sql-lg-bics.database.windows.net"
 database = "db_az_sql_20250619"
 username = "sqladmin"
-password = os.getenv("SQL_PASSWORD")  # Securely read from environment
 
-if not password:
-    raise ValueError("SQL_PASSWORD environment variable is not set")
+parser = argparse.ArgumentParser()
+parser.add_argument('--sql-password', required=True)
+args = parser.parse_args()
+
+password = args.sql_password
 
 mlflow.set_tracking_uri(
     f"mssql+pymssql://{username}:{password}@{server}:1433/{database}"
 )
 
 print("Tracking URI is now:", mlflow.get_tracking_uri())
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--sql-password', required=True)
+args = parser.parse_args()
+
+password = args.sql_password
